f2e2d9de597a3cf075b8017d751c58e98510319d
LakshyaTecno <LakshyaTecno@gmail.com>
2022-07-08 12:47:59 +0530

    jwt

---
+++ src/util/auth.js
@@ -0,0 +1,68 @@
+const passport = require("passport");
+const localStrategy = require("passport-local");
+const User = require("../models/user");
+const JWTStrategy = require("passport-jwt").Strategy;
+const ExtractJWT = require("passport-jwt").ExtractJwt;
+
+passport.use(
+  "signup",
+  new localStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async (email, password, done) => {
+      try {
+        const user = await User.create({ email, password });
+        return done(null, user);
+      } catch (err) {
+        console.log(err);
+        done(err);
+      }
+    }
+  )
+);
+
+passport.use(
+  "login",
+  new localStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async (email, password, done) => {
+      try {
+        const user = await User.findOne({ email });
+        if (!user) {
+          return done(null, false, { message: "User Not found" });
+        }
+        const validate = await user.isvalidPassword(password);
+        if (!validate) {
+          return done(null, false, { message: "Password is not valid" });
+        }
+
+        return done(null, user, { message: "Login Successfully" });
+      } catch (err) {
+        console.log(err);
+        done(err);
+      }
+    }
+  )
+);
+
+passport.use(
+  new JWTStrategy(
+    {
+      secretOrKey: "TOP_SECRET",
+      jwtFromRequest: ExtractJWT.fromUrlQueryParameter("secret_token"),
+    },
+    async (token, done) => {
+      try {
+        return done(null, token.user);
+      } catch (err) {
+        console.log(err);
+        done(err);
+      }
+    }
+  )
+);
